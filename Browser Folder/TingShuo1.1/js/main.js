c58b5069a4fa79183dfbb87ae0768950b994eaed
ISheenSomeOne <39010005+ISheenSomeOne@users.noreply.github.com>
2018-09-04 00:05:42 +0800

    Add files via upload

--- Browser Folder/TingShuo1.1/js/main.js
+++ Browser Folder/TingShuo1.1/js/main.js
@@ -1,6 +1,21 @@
+
+
 function byId(id) {
     return document.getElementById(id);
 }
 function byClass (cl) {
     return document.getElementsByClassName(cl);
+}
+let first = "http://localhost:8080/TestAPI";
+function GetRequest() {   
+    var url = location.search; //获取url中"?"符后的字串   
+    var theRequest = new Object();   
+    if (url.indexOf("?") != -1) {   
+        var str = url.substr(1);   
+        strs = str.split("&");   
+        for(var i = 0; i < strs.length; i ++) {   
+            theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);   
+        }   
+    }   
+    return theRequest;   
 }
