4b9b42cb582e239f1199ab0ca448dfa9a5a3bb49
JackWang <674112795@qq.com>
2018-09-07 15:44:00 +0800

    项目最终版本

--- Server Folder Back up/SSLearningTeam/src/cn/net/realloyal/core/common/Anonymous.java
+++
@@ -1,8 +0,0 @@
-package cn.net.realloyal.core.common;
-
-/**
- * 匿名接口，用于跳过登录拦截器
- */
-public interface Anonymous {
-
-}
--- Server Folder Back up/SSLearningTeam/src/cn/net/realloyal/core/util/CalculateStartIndex.java
+++
@@ -1,8 +0,0 @@
-package cn.net.realloyal.core.util;
-
-public class CalculateStartIndex {
-	//用于计算指定页面开始的下标
-	public static int getStartIndex(int startIndex,int numInAPage) {
-		return (startIndex-1)*numInAPage;
-	}
-}
